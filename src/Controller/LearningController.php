73dcc7bf3b02ada33d0fc1049639d78933af0e6a
DHIRAJ_GURUNG <grgdhiraj.be@gmail.com>
2020-03-03 17:01:14 +0100

    final commit

--- src/Controller/LearningController.php
+++ src/Controller/LearningController.php
@@ -7,13 +7,45 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LearningController extends AbstractController
 {
+    private $name;
     /**
      * @Route("/learning", name="learning")
      */
-    public function index()
+    public function aboutMe()
     {
         return $this->render('learning/index.html.twig', [
             'controller_name' => 'LearningController',
         ]);
     }
+
+    public function showMyName(){
+       if (!isset($_POST['name'])){
+         $_POST ['name'] = 'there! Angel of my nightmare 😇';
+       }
+
+        return $this->render('learning/showMyName.html.twig', [
+            'controller_name' => 'LearningController', "name" => $_POST ['name']
+        ]);
+    }
+
+    private function name() {
+        return $this->name;
+    }
+
+    public function changeMyName() {
+    if (!isset($_POST['name'])) {
+        $_POST['name']= "undefined";
+    }
+    $this->name = $_POST['name'];
+    return $this->render('learning/changeMyName.html.twig', [
+        'controller_name' => 'LearningController',
+        'name' => $this->name()
+    ]);
 }
+
+
+
+}
+
+
+
